feec8c4244f582ec1086160994560cced3d73eda
Aboubakar Sidik Faha <joefaha1@gmail.com>
2022-05-23 13:37:33 +0100

    feat: project configuration

---
+++ src/views/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <>
+      <div className="App">
+        <div className="container flex flex-col items-center justify-center h-screen">
+          <h1 className="text-blue-600 font-black">Hello</h1>
+        </div>
+      </div>
+    </>
+  );
+}
