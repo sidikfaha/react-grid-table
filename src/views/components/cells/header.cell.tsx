c18462081c6d03dec3dd99121282452f208e0fed
Aboubakar Sidik Faha <joefaha1@gmail.com>
2022-05-26 06:23:44 +0100

    feat: header arrow down

--- src/views/components/cells/header.cell.tsx
+++ src/views/components/cells/header.cell.tsx
@@ -1,31 +1,41 @@
-import { Cell, CellTemplate, Compatible, getCellProperty, Uncertain } from "@silevis/reactgrid"
-import React from "react"
+import {
+  Cell,
+  CellTemplate,
+  Compatible,
+  getCellProperty,
+  Uncertain,
+} from "@silevis/reactgrid";
+import React from "react";
+import { IoChevronDown } from "react-icons/io5";
 
 export interface HeaderIconCell extends Cell {
-    type: 'headerIcon',
-    text: string,
-    icon?: React.ReactNode,
+  type: "headerIcon";
+  text: string;
+  icon?: React.ReactNode;
 }
 
-export class HeaderIconCellTemplate implements CellTemplate<Cell | HeaderIconCell> {
-    isFocusable = (cell: Compatible<HeaderIconCell>): boolean => false;
+export class HeaderIconCellTemplate
+  implements CellTemplate<Cell | HeaderIconCell>
+{
+  isFocusable = (cell: Compatible<HeaderIconCell>): boolean => false;
 
-    getCompatibleCell(uncertainCell: Uncertain<HeaderIconCell>): Compatible<HeaderIconCell> {
-        const text = getCellProperty(uncertainCell, "text", "string");
-        const icon = getCellProperty(uncertainCell, "icon", "object");
-        const value = parseFloat(text);
-        return { ...uncertainCell, text, value, icon };
-    }
-    render(cell: Compatible<HeaderIconCell>) {
-        return (
-            <div className="flex gap-3 items-center grid-header">
-                <div>
-                    {cell.icon}
-                </div>
-                <div>
-                    {cell.text}
-                </div>
-            </div>
-        )
-    }
+  getCompatibleCell(
+    uncertainCell: Uncertain<HeaderIconCell>
+  ): Compatible<HeaderIconCell> {
+    const text = getCellProperty(uncertainCell, "text", "string");
+    const icon = getCellProperty(uncertainCell, "icon", "object");
+    const value = parseFloat(text);
+    return { ...uncertainCell, text, value, icon };
+  }
+  render(cell: Compatible<HeaderIconCell>) {
+    return (
+      <>
+        <div className="flex gap-3 items-center grid-header">
+          <div>{cell.icon}</div>
+          <div>{cell.text}</div>
+        </div>
+        <IoChevronDown />
+      </>
+    );
+  }
 }
